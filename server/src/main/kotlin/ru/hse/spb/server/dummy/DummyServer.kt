894cdaaab9475385bb145d9772726106322fa329
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2019-01-15 21:22:16 +0300

    Add error handling to dummy server

--- server/src/main/kotlin/ru/hse/spb/server/dummy/DummyServer.kt
+++ server/src/main/kotlin/ru/hse/spb/server/dummy/DummyServer.kt
@@ -5,45 +5,61 @@ import ru.hse.spb.common.generateMessage
 import ru.hse.spb.message.ProtoBuf
 import ru.hse.spb.server.Server
 import ru.hse.spb.server.common.sortReceivedList
+import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.ServerSocket
 import java.net.Socket
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 
 class DummyServer : Server {
     override fun runServer(address: InetSocketAddress) {
         val serverSocket = ServerSocket(address.port)
         val threadPool = Executors.newCachedThreadPool()
         while (true) {
-            val socket = serverSocket.accept()
-            threadPool.submit(Worker(socket))
+            try {
+                val socket = serverSocket.accept()
+                threadPool.submit(Worker(socket))
+            } catch (e: IOException) {
+                println("Can not accept client: ${e.message}")
+            }
         }
     }
 }
 
-class Worker(private val socket: Socket): Runnable {
+class Worker(private val socket: Socket) : Runnable {
+    companion object {
+        val idCounter = AtomicInteger()
+    }
+
+    private val id = idCounter.getAndIncrement()
+
     override fun run() {
-        while (true) {
-            val receivedMessage = ProtoBuf.Message.parseDelimitedFrom(socket.inputStream)
+        try {
+            while (true) {
+                val receivedMessage = ProtoBuf.Message.parseDelimitedFrom(socket.inputStream)
 
-            // handle request
-            val startTime = System.currentTimeMillis()
-            val (sortingTime, sortedList) = sortReceivedList(receivedMessage)
-            val message = sortedList.generateMessage()
-            val endTime = System.currentTimeMillis()
+                // handle request
+                val startTime = System.currentTimeMillis()
+                val (sortingTime, sortedList) = sortReceivedList(receivedMessage)
+                val message = sortedList.generateMessage()
+                val endTime = System.currentTimeMillis()
 
-            // send response
-            message.writeDelimitedTo(socket.outputStream)
+                // send response
+                message.writeDelimitedTo(socket.outputStream)
 
-            // send metrics
-            val metrics = ProtoBuf.Metrics.newBuilder()
-                .setSortingTime(sortingTime)
-                .setRequestTime(endTime - startTime)
-                .build()
-            metrics.writeDelimitedTo(socket.outputStream)
-            if (!receivedMessage.hasNextRequest) {
-                break
+                // send metrics
+                val metrics = ProtoBuf.Metrics.newBuilder()
+                    .setSortingTime(sortingTime)
+                    .setRequestTime(endTime - startTime)
+                    .build()
+                metrics.writeDelimitedTo(socket.outputStream)
+                if (!receivedMessage.hasNextRequest) {
+                    break
+                }
             }
+        } catch (e: IOException) {
+            println("We lost client $id: ${e.message}")
         }
     }
 }
